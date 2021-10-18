a1d1c05ccc635ae319f0525aecd5557ecd01f035
enesozmert <en-es111@hotmail.com>
2021-10-19 00:00:54 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,30 @@
+console.log("Merhaba Kodlama.io")
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+{
+    let dolarDun = 9.10;
+}
+console.log(dolarDun);
+const euroDun = 11.2
+//euroDun=11
+//console.log(euroDun);
+
+let konutKredileri = ["Konur Kredisi" , "Emlak Konut Kredisi" , "Kamu Konut Kredisi"]
+//array
+
+console.log("<ul>");
+for (let index = 0; index < konutKredileri.length; index++) {
+    
+    console.log("<li>"+konutKredileri[index]+"</li>");
+}
+console.log("</ul>");
+{/* <ul>
+    <li>Konut Kredisi</li>
+    <li>Emlak Konut Kredisi</li>
+    <li>Kamu</li>
+</ul> */}
+
+console.log(konutKredileri);
+
+//camelCase
+//PascalCase
